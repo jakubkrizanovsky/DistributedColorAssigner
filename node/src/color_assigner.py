0058698ba3524592aa4174dcb85b61b4708d030d
Jakub Krizanovsky <jakub.krizanovsky@gmail.com>
2025-03-23 14:32:14 +0100

    Count and compare actual and target distributions

--- node/src/color_assigner.py
+++ node/src/color_assigner.py
@@ -18,9 +18,11 @@ class ColorAssigner(Thread):
 
         self.randomize_color()
 
+
     def set_color(self, color:str):
         self.color = color
         self.color_table[self.communication.own_addr + " (this node)"] = color
+
 
     def randomize_color(self):
         total = sum(COLOR_DITRIBUTION.values())
@@ -31,25 +33,78 @@ class ColorAssigner(Thread):
                 self.set_color(color)
                 return
 
-    def update(self, node, msg):
+
+    def update_loop(self):
+        while True:
+            target_distribution = self.get_target_distribution()
+            actual_distribution = self.get_actual_distribution()
+            print(f"Is target distribution: {self.is_target_distribution(target_distribution, actual_distribution)}")
+            self.randomize_color()
+            self.send_color()
+
+
+    def get_target_distribution(self) -> Dict[str, int]:
+        target_distribution: Dict[str, int] = {}
+        node_count = len(self.color_table)
+        total = sum(COLOR_DITRIBUTION.values())
+        actual_sum = 0
+        
+        for color, dist in COLOR_DITRIBUTION.items():
+            x = int(dist / total * node_count)
+            target_distribution[color] = x
+            actual_sum += x
+
+        if actual_sum < node_count:
+            target_distribution[FILL_COLOR] += node_count - actual_sum
+
+        print("Target distribution")
+        for color, value in target_distribution.items():
+            print(f"{color} : {value}")
+
+        return target_distribution
+    
+    
+    def get_actual_distribution(self) -> Dict[str, int]:
+        actual_distribution: Dict[str, int] = {}
+        for color in COLOR_DITRIBUTION.keys():
+            actual_distribution[color] = 0
+
+        for color in self.color_table.values():
+            actual_distribution[color] += 1
+
+        print("Actual distribution")
+        for color, value in actual_distribution.items():
+            print(f"{color} : {value}")
+
+        return actual_distribution
+
+
+    def is_target_distribution(self, target_distribution:Dict[str, int], actual_distribution:Dict[str, int]) -> bool:
+        for color, count in target_distribution.items():
+            if(count != actual_distribution[color]):
+                return False
+            
+        return True
+
+
+    def update_table(self, node, msg):
         if(msg["type"]) == "update":
             self.color_table[node] = msg["value"]
 
+
     def send_color(self):
-        while True:
-            self.randomize_color()
+        msg = {
+            "type":"update",
+            "value":self.color
+        }
 
-            msg = {
-                "type":"update",
-                "value":self.color
-            }
+        self.communication.send(msg)
+        time.sleep(1)
 
-            self.communication.send(msg)
-            time.sleep(1)
-    
+
     def run(self):
-        listen_thread = Thread(target=self.communication.listen, args=[self.update])
-        send_thread = Thread(target=self.send_color)
+        listen_thread = Thread(target=self.communication.listen, args=[self.update_table])
+        send_thread = Thread(target=self.update_loop)
 
         listen_thread.start()
         send_thread.start()
--- node/src/config.py
+++ node/src/config.py
@@ -3,6 +3,8 @@ COLOR_DITRIBUTION = {
     "green": 40
 }
 
+FILL_COLOR = "green"
+
 MCAST_PORT:int = 8888
 MCAST_ADDR:str = "224.1.1.1"
 MCAST_TTL = 1
